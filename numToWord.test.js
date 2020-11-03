fe5261dc24197fb5c3bae30e22823e57808d72dc
KAS-Repo <73745061+KAS-Repo@users.noreply.github.com>
2020-11-03 22:34:17 +0100

    Update numToWord.test.js
    Explaining using tests with Jest library, situation and problems

--- numToWord.test.js
+++ numToWord.test.js
@@ -7,3 +7,8 @@ test("Should output an English phrase equivalent to numerical one", () => {
   const number = toEnglish(1999);
   expect(number).toBe("one thousand nine hundred ninety-nine");
 });
+
+
+//This is a mock for Jest environment
+//Jest do not support import (ES6) right now so for using this test I should have write the whole programm in Node.js
+//so that I could use require in here, but I made another test using Jasmine in Test Folder which is better to setup in vanilla JavaScript
